016fd912bce1013a9b9ca87141a9f5f02fdf448b
Avrahman <aiguernazene@gmail.com>
2023-02-08 15:38:42 +0100

    Le spacsh

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const page = document.querySelector('.page');
+const logo = document.querySelector('.logo');
+const title = document.querySelector('.title');
+const h1 = document.querySelector('h1');
+
+function alerter() {
+//    alert("Bonjour !")
+}
+page.addEventListener('click', alerter());
